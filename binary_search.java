1ced169728a2d6a98971ada6b84d3bc61edf6fed
sapphire197 <120254540+sapphire197@users.noreply.github.com>
2024-09-29 00:24:47 +0530

    Update binary_search.java

--- binary_search.java
+++ binary_search.java
@@ -1,8 +1,6 @@
 import java.util.*;
 public class binary_search {
-
     public static int binary_search(int numbers[], int key){
-
         int start = 0; int end = numbers.length-1;
         while (start <= end) { 
             int mid = ((start+end)/2);
@@ -20,11 +18,9 @@ public class binary_search {
         }
         return -1;        
     }
-    
     public static void main(String args[]) {
     int numbers[] = {2,4,6,8,10,12,14,16};
     int key = 4;
     System.out.println("Your key is at :  "+binary_search(numbers,key));
     }
 }
-    
