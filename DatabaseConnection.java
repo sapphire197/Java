53b3dd04c18e7f226428b13a199b9d4686429f0e
sapphire197 <annaalagu19@gmail.com>
2024-10-27 21:48:00 +0530

    DatabaseConnection.java file added

---
+++ DatabaseConnection.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/book_inventory";
+    private static final String USER = "root";
+    private static final String PASSWORD = "your_password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
