2d1385c0327e1c8f4ec781e9f25387ccea3e62fb
sapphire197 <annaalagu19@gmail.com>
2024-10-28 19:57:47 +0530

    book.java file updated

--- Book.java
+++ Book.java
@@ -6,14 +6,71 @@ public class Book {
     private int publicationYear;
     private boolean isBorrowed;
 
-    // Constructor, getters, and setters
+    // Constructor for creating new books (without id)
     public Book(String title, String author, String genre, int publicationYear) {
         this.title = title;
         this.author = author;
         this.genre = genre;
         this.publicationYear = publicationYear;
-        this.isBorrowed = false;
+        this.isBorrowed = false; // Default value for new books
+    }
+
+    // Constructor for retrieving books from database (with id)
+    public Book(int id, String title, String author, String genre, int publicationYear, boolean isBorrowed) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.publicationYear = publicationYear;
+        this.isBorrowed = isBorrowed;
     }
 
     // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        isBorrowed = borrowed;
+    }
 }
