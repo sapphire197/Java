29583a52c813e0ab087689a81abcef57c72664b3
sapphire197 <annaalagu19@gmail.com>
2024-10-29 23:23:07 +0530

    new project quiz

---
+++ Question.java
@@ -0,0 +1,27 @@
+public class Question {
+    private String questionText;
+    private String[] options;
+    private String correctAnswer;
+
+    public Question(String questionText, String[] options, String correctAnswer) {
+        this.questionText = questionText;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public boolean isCorrect(String answer) {
+        return correctAnswer.equals(answer);
+    }
+}
