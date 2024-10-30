a60eb34a7f75c6cbfa59b2fcf059910a39871a86
sapphire197 <annaalagu19@gmail.com>
2024-10-30 23:42:29 +0530

    ATM GUI Project

--- ATMSystem.sql
+++ ATMSystem.sql
@@ -1,17 +1,18 @@
 CREATE DATABASE ATMSystem;
-
 USE ATMSystem;
 
 CREATE TABLE accounts (
-    account_id INT PRIMARY KEY AUTO_INCREMENT,
-    balance DOUBLE NOT NULL
+    account_id INT PRIMARY KEY,
+    balance DOUBLE
 );
 
 CREATE TABLE transactions (
-    transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+    transaction_id INT AUTO_INCREMENT PRIMARY KEY,
     account_id INT,
-    type VARCHAR(10),
+    type VARCHAR(50),
     amount DOUBLE,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (account_id) REFERENCES accounts(account_id)
 );
+
+INSERT INTO accounts (account_id, balance) VALUES (1, 1000.0);
