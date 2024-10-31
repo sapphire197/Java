af05ab2020bef2c7772834d4386963f1629bfc29
sapphire197 <120254540+sapphire197@users.noreply.github.com>
2024-11-01 00:26:13 +0530

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -1,3 +1,6 @@
+/*Database Connection Utility
+This utility handles connections to the MySQL database.*/
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
